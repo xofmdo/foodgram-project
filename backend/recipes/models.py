db8e29880f47b134d698bb067b87f955d3390926
Alexandr Kuzmich <92593750+xofmdo@users.noreply.github.com>
2022-11-05 02:47:57 +0300

    First step. Creation of applications and some models

---
+++ backend/recipes/models.py
@@ -0,0 +1,122 @@
+from django.contrib.auth import get_user_model
+from django.core.validators import (MinValueValidator, MaxValueValidator)
+from django.db import models
+
+User = get_user_model()
+
+
+class Tag(models.Model):
+    """Модель тега"""
+    title = models.CharField(
+        max_length=50,
+        unique=True,
+        default="#ffffff",
+        verbose_name='Название тэга')
+    hexcolor = models.CharField(
+        max_length=7,
+        default="#ffffff",
+        verbose_name='Цветовой HEX-код')
+    slug = models.SlugField(
+        max_length=100,
+        unique=True,
+        verbose_name='Уникальный слаг')
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
+    def __str__(self):
+        return self.title
+
+
+class Ingredient(models.Model):
+    """Модель ингридиента"""
+    title = models.CharField(
+        max_length=200,
+        verbose_name='Название рецепта')
+
+    units = models.CharField(
+        max_length=200,
+        verbose_name='Единицы измерения')
+
+    class Meta:
+        verbose_name = 'Ингредиент'
+        verbose_name_plural = 'Ингредиенты'
+
+    def __str__(self):
+        return f'{self.title}, {self.units}'
+
+
+class Recipe(models.Model):
+    author = models.ForeignKey(
+        User,
+        related_name='recipes',
+        on_delete=models.CASCADE,
+        verbose_name='Автор',
+    )
+    title = models.CharField(
+        max_length=200,
+        verbose_name='Название ингридиента'
+    )
+    image = models.ImageField(
+        verbose_name='Картинка',
+        upload_to='recipes/',
+        blank=True
+    )
+    description = models.TextField(
+        verbose_name='Описание рецепта'
+    )
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        through='IngredientInRecipe',
+        related_name='recipes',
+        verbose_name='Ингредиенты'
+    )
+    tags = models.ManyToManyField(
+        Tag,
+        related_name='recipes',
+        verbose_name='Теги'
+    )
+    cooking_time = models.PositiveSmallIntegerField(
+        'Время приготовления',
+        validators=[
+            MinValueValidator(1, message='Минимальное значение 1!'),
+            MaxValueValidator(300, message='Максимальное значение 300!')
+        ]
+    )
+
+    class Meta:
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
+
+
+class IngredientInRecipe(models.Model):
+    """Модель количества ингридиентов в отдельных рецептах"""
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='ingredient_list',
+        verbose_name='Рецепт',
+    )
+    ingredient = models.ForeignKey(
+        Ingredient,
+        on_delete=models.CASCADE,
+        verbose_name='Ингредиент',
+    )
+    amount = models.PositiveSmallIntegerField(
+        'Количество',
+        validators=[
+            MinValueValidator(1, message='Минимальное количество 1!'),
+            MaxValueValidator(300,
+                              message='Максимальное значение 300!')
+        ]
+    )
+
+    class Meta:
+        verbose_name = 'Ингредиент в рецепте'
+        verbose_name_plural = 'Ингредиенты в рецептах'
+
+    # def __str__(self):
+    #    return
+
+
