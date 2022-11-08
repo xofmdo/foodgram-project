e3e65082c12e62cb3c104d33b5a54760b344d91e
Alexandr Kuzmich <92593750+xofmdo@users.noreply.github.com>
2022-11-08 21:51:45 +0300

    fixed bugs

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -136,6 +136,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
+
+    'PAGE_SIZE': 6,
 }
 
 SIMPLE_JWT = {
