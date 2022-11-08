ec14c426bfa07ff187f9a15c83c75ca25e48afe9
Alexandr Kuzmich <92593750+xofmdo@users.noreply.github.com>
2022-11-08 20:03:28 +0300

    Added admin info

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,3 +1,51 @@
-from django.contrib import admin
+from django.contrib.admin import ModelAdmin, register
 
-# Register your models here.
+from .models import Ingredient, IngredientInRecipe, Recipe, Tag
+
+
+@register(Ingredient)
+class IngredientAdmin(ModelAdmin):
+    list_display = (
+        'title', 'units',
+    )
+    search_fields = (
+        'title',
+    )
+    list_filter = (
+        'title',
+    )
+
+
+@register(Recipe)
+class RecipeAdmin(ModelAdmin):
+    list_display = (
+        'title', 'author',
+    )
+    fields = (
+        ('title', 'cooking_time',),
+        ('author', 'tags',),
+        ('description',),
+        ('image',),
+    )
+    raw_id_fields = ('author', )
+    search_fields = (
+        'title', 'author',
+    )
+    list_filter = (
+        'title', 'author__username',
+    )
+
+
+@register(Tag)
+class TagAdmin(ModelAdmin):
+    list_display = (
+        'title', 'color', 'slug',
+    )
+    search_fields = (
+        'title', 'color'
+    )
+
+
+@register(IngredientInRecipe)
+class IngredientInRecipe(ModelAdmin):
+    pass
