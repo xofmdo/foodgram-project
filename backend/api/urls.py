22d994a8becce9f832cb534f34c05d74fb87a3b2
Alexandr Kuzmich <92593750+xofmdo@users.noreply.github.com>
2022-11-08 21:43:02 +0300

    Created some urls

---
+++ backend/api/urls.py
@@ -0,0 +1,17 @@
+from django.urls import include, path
+from rest_framework import routers
+
+from .views import (
+    IngredientViewSet,
+    TagViewSet
+)
+
+router = routers.DefaultRouter()
+router.register('tags', TagViewSet, basename='tags')
+router.register('ingredients', IngredientViewSet, basename='ingredients')
+
+app_name = 'api'
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
