b1611fb9a10b3a344a2b13dd77ffced3d11feb25
Alexandr Kuzmich <92593750+xofmdo@users.noreply.github.com>
2022-11-08 21:46:51 +0300

    Created some views

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from backend.recipes.models import (
+    Ingredient,
+    IngredientInRecipe,
+    Recipe,
+    Tag
+)
+from .serializer import (
+    TagSerializer,
+    IngredientSerializer,
+    IngredientInRecipeSerializer,
+    RecipeSerializer,
+    AddIngredientSerializer,
+)
+
+
+class TagViewSet(viewsets.ReadOnlyModelViewSet):
+    """Вьюсет работы с обьектами класса Tag."""
+
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    permission_classes = (AllowAny,)
+    pagination_class = None
+
+
+class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
+    """Вьюсет для работы с обьектами класса Ingredient."""
+
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
+    permission_classes = (AllowAny,)
+    search_fields = ('^name', )
+    pagination_class = None
