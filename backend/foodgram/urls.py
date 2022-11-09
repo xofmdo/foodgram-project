09725abe6d869f7efc69bfc51f18b999a95f3552
Alexandr Kuzmich <92593750+xofmdo@users.noreply.github.com>
2022-11-09 13:17:55 +0300

    fix bugs

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("users.urls", namespace='users')),
+    path('api/', include("users.urls")),
 
 ]
