b58b5541ee8a708b7ca3ff62bf4a5eeb7557185f
Alexandr Kuzmich <92593750+xofmdo@users.noreply.github.com>
2022-11-11 12:55:29 +0300

    Fixed users model

--- backend/users/models.py
+++ backend/users/models.py
@@ -9,12 +9,23 @@ class User(AbstractUser):
         verbose_name='Электронная почта',
         unique=True
     )
+    username = models.CharField(
+        max_length=150,
+        verbose_name='Имя пользователя',
+        unique=True,
+        db_index=True
+    )
+    first_name = models.CharField(
+        max_length=150,
+        verbose_name='Имя'
+    )
+    last_name = models.CharField(
+        max_length=150,
+        verbose_name='Фамилия'
+    )
+
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
-
-    class Meta:
-        verbose_name = 'Пользователь'
-        verbose_name_plural = 'Пользователи'
 
     class Meta:
         verbose_name = 'Пользователь'
